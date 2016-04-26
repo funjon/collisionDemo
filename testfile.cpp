7bc3f8e3214a2621e656291fd7712a5b96392702
Jonathan Disher <jdisher@fuezakyuu.local>
2016-04-25 18:02:21 -0700

    initial creation ; setting up a demo file

---
+++ testfile.cpp
@@ -0,0 +1,21 @@
+/*
+ * testfile.cpp
+ * 
+ * demo file for git collision detection and resolution
+ */
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, const char* argv[]) {
+  int variable = 0;
+
+  while (variable < 5) {
+    cout << "The variable is " << variable << endl;
+    variable++;
+  }
+
+  return 0;
+}
